68441f3d4e884ea98e443b770b20f21cdb1a593d
java2019 <java_2019@mail.ru>
2017-07-02 19:07:14 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/camera/supportedmodes/CameraInfoActivity.java
@@ -0,0 +1,13 @@
+package com.samples.camera.supportedmodes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_info);
+    }
+}
